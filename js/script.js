48b01b271b50ba3e93fc253591711629eb63e0a5
Katie Siegel <165830396+Katie-Siegel@users.noreply.github.com>
2025-04-04 15:47:31 -0400

    Added repos display to page

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ const bigPicture =
 
 //Information to include in bigPicture//
 const username = 'Katie-Siegel';
+//Div where repos will be displayed//
+const reposList =
+	document.querySelector('.repo-list');
 
+/////////////Functions to poulate the bio section (class=overview)///////////
 //Connects page to Git Hub API//
 const getInfo = async function () {
 	const results = await fetch(
@@ -29,7 +33,29 @@ const displayInfo = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
 	bigPicture.append(div);
+	getRepos();
 };
 
 //Calls getInfo() function//
 getInfo();
+
+/////////Functions to populate the list of repositories (class=repo-list)//////////////
+//Fetches Repos//
+const getRepos = async function () {
+	const results = await fetch(
+		`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+	);
+	const data = await results.json();
+	console.log(data);
+	displayRepos(data);
+};
+
+//Displays the results of getRepos() on the page//
+const displayRepos = function (repos) {
+	repos.forEach(function (repo) {
+		let repoItem = document.createElement('h3');
+		repoItem.classList.add('h3');
+		repoItem.innerText = `${repo.name}`;
+		reposList.append(repoItem);
+	});
+};
